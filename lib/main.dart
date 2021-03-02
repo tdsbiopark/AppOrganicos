aa2fdc2c97ac05e31ea3d1ff0f584196e4f1042f
Andre Lucyo <andre.lucyo2@gmail.com>
2021-03-01 21:24:25 -0300

    Corrigido erro de conexao com servidor postgres, app compilando com sucesso

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'App Organicos IFPR',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
