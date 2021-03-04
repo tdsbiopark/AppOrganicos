4a92a2eea2c7887c6a46bc206bb95a62b30b5bca
Jonas Sidney <jonasidney@hotmail.com>
2021-03-04 00:57:00 -0300

    Projeto base, branch DevJonas - WIP

--- app_organicos/lib/dao/conexao.dart
+++ app_organicos/lib/dao/conexao.dart
@@ -5,8 +5,8 @@ class Conexao {
 
   static Future<PostgreSQLConnection> getConexao() async {
     if (_conexaoPostgreSQL == null || _conexaoPostgreSQL.isClosed) {
-      _conexaoPostgreSQL = new PostgreSQLConnection("10.0.0.126", 5432, "organicos",
-          username: "postgres", password: "postgres");
+      _conexaoPostgreSQL = new PostgreSQLConnection("192.168.25.22", 5432, "organicos",
+          username: "postgres", password: "s1st3m@s");
       await _conexaoPostgreSQL.open();
     }
 
