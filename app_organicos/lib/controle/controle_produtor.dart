da561727833ab440c425c97f160b64cf5c92ac31
Andre Lucyo <andre.lucyo2@gmail.com>
2021-03-09 00:45:57 -0300

    Implementado o ControleProduto, criado arquivos de classe dari para as Telas.

---
+++ app_organicos/lib/controle/controle_produtor.dart
@@ -0,0 +1,23 @@
+import 'package:app_organicos/dao/dao_produtor.dart';
+import 'package:app_organicos/modelo/produtor.dart';
+
+class ControleProdutor {
+  //DAO:
+  ProdutorDao _dao = ProdutorDao();
+  //Obj
+  Produtor produtorEmEdicao = Produtor();
+  //Lista Futura:guarda o que vem do banco
+  Future<List<Produtor>> fProdutoresPesquisados;
+  //Lista para uso em tela ()Biding, reflete a pesquisa
+  List<Produtor> produtoresPesquisados;
+
+  //Metodo assincrono para gravar:
+  Future<void> gravarProdutoEmEdicao() async {
+    await _dao.gravar(produtorEmEdicao);
+  }
+
+  //Metodo assincrono para pesquisar: opcional recebe um txt para pesquisa
+  Future<void> atualizarPesquisa({String filtro = ""}) {
+    fProdutoresPesquisados = _dao.pesquisar(filtro);
+  }
+}
