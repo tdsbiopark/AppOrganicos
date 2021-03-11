40506df9399c2a824f8edbd520f00dee6171a898
Andre Lucyo <andre.lucyo2@gmail.com>
2021-03-11 00:03:05 -0300

    criado nova branch apos merge da master.

--- app_organicos/lib/main.dart
+++ app_organicos/lib/main.dart
@@ -17,8 +17,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: TelaPesquisaProdutor(),
-      //home: TelaPesquisaProduto(),
+      //home: TelaPesquisaProdutor(),
+      home: TelaPesquisaProduto(),
     );
   }
 }
