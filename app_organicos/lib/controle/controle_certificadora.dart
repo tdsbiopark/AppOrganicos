51e81ed94afbfc29e36534f6dca0bdce94a96826
Jonas Sidney <jonasidney@hotmail.com>
2021-03-10 21:22:32 -0300

    Liberação das Classes DAO_Certificadora e Controle_Certificadora

---
+++ app_organicos/lib/controle/controle_certificadora.dart
@@ -0,0 +1,17 @@
+import 'package:app_organicos/dao/dao_certificadora.dart';
+import 'package:app_organicos/modelo/certificadora.dart';
+
+class ControleCertificadora {
+  CertificadoraDAO _dao = CertificadoraDAO();
+  Certificadora certificadoraEmEdicao = Certificadora();
+  Future<List<Certificadora>> fCertificadorasPesquisadas;
+  List<Certificadora> certificadorasPesquisadas;
+
+  Future<void> gravarCertificadoraEmEdicao() async {
+    await _dao.gravar(certificadoraEmEdicao);
+  }
+
+  Future<void> atualizarPesquisa({String filtro = ""}) {
+    fCertificadorasPesquisadas = _dao.pesquisar(filtro);
+  }
+}
