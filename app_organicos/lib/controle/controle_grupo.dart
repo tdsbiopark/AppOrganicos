9929690450a51c7f1750b11d0a72ec47bcee8a79
Maycon Antunes <mayconjr09@hotmail.com>
2021-04-16 22:27:09 -0300

    Ajuste do Erro em Controle_grupo.dart

--- app_organicos/lib/controle/controle_grupo.dart
+++ app_organicos/lib/controle/controle_grupo.dart
@@ -1,17 +1,20 @@
-import 'package:app_organicos/dao/dao_Grupo.dart';
-import 'package:app_organicos/modelo/Grupo.dart';
+import 'package:app_organicos/dao/dao_grupo.dart';
+import 'package:app_organicos/modelo/grupo.dart';
 
 class ControleGrupo {
-  GrupoDAO _dao = GrupoDAO();
+  GrupoDao _dao = GrupoDao();
+
   Grupo grupoEmEdicao = Grupo();
-  Future<List<Grupo>> fGruposPesquisadas;
-  List<Grupo> GruposPesquisadas;
+
+  Future<List<Grupo>> fGruposPesquisados;
+
+  List<Grupo> GruposPesquisados;
 
   Future<void> gravarGrupoEmEdicao() async {
     await _dao.gravar(grupoEmEdicao);
   }
 
-  Future<Grupo> atualizarPesquisa({String filtro = ""}) {
-    fGruposPesquisadas = _dao.pesquisar(filtro);
+  Future<void> atualizarPesquisa({String filtro = ""}) {
+    fGruposPesquisados = _dao.pesquisar(filtro);
   }
 }
