a95ce596165ca8a2831863f417ad0ba2276d1929
Andre Lucyo <andre.lucyo2@gmail.com>
2021-04-18 09:04:39 -0300

    Resolvido issue #11 , criado metodo equals no modelo de grupo

--- app_organicos/lib/modelo/grupo.dart
+++ app_organicos/lib/modelo/grupo.dart
@@ -11,4 +11,10 @@ class Grupo {
   String incricaoEstadual;
   String distribuidor;
   bool ativo = true;
+
+  //para poder percorrer na combobox na tela de cadastro
+  //tem que ser um tipo e o id deve ser igual
+  bool operator ==(other) {
+    return (other is Grupo && other.id == this.id);
+  }
 }
